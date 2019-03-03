275c2a0429fbf57b9c62f0f4bf59e0adee7a86fd
makk <mkahack@hawk.iit.edu>
2019-03-02 22:40:25 -0600

    Almost done with homework 2

--- helper.py
+++ helper.py
@@ -11,3 +11,33 @@ def my_gini(panda, col):
     for i in range(len(prob_array)):
         p+=np.square(prob_array[names[i]])
     return 1 - p
+
+def weighted_gini(parent, child, col):
+    weight = child[col].count()/parent[col].count()
+    return weight*my_gini(child, col)
+
+def my_entropy(panda, col):
+    p = 0
+    names = []
+    grouping = panda.groupby(col)[col]
+    pb = grouping.count()/panda[col].count()
+    for name, _ in grouping:
+        names.append(name)
+    for i in range(len(names)):
+        p+= pb[names[i]]*np.log2(pb[names[i]])
+    return -p
+
+def information_gain(panda, right, left, col):
+    child_ent = (right[col].count()/panda[col].count())*my_entropy(right, col) + (left[col].count()/panda[col].count())*my_entropy(left, col)
+    return my_entropy(panda, col) - child_ent
+
+def my_misclass(panda, col):
+    p = 0
+    names = []
+    grouping = panda.groupby(col)[col]
+    prob_array = grouping.count()/panda[col].count()
+    return 1 - max(prob_array)
+
+def weighted_misclass(parent, child, col):
+    weight = child[col].count()/parent[col].count()
+    return weight*my_misclass(child, col)
