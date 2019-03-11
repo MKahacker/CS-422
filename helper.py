2fd2f0252dc83b8ab3639005eafbfc1432e4e2a7
makk <mkahack@hawk.iit.edu>
2019-03-11 16:36:42 -0500

    Finished HWW3

--- helper.py
+++ helper.py
@@ -52,3 +52,41 @@ def my_misclass(panda, col):
 def weighted_misclass(parent, child, col):
     weight = child[col].count()/parent[col].count()
     return weight*my_misclass(child, col)
+
+def frequent_set(can,trans, i):
+    freq = []
+    for c in candidate[i]:
+        sum_c = 0
+        for t in trans:
+            if c.issubset(t['items']):
+                sum_c = sum_c +1
+        sup = sum_c/len(transactions)
+        #print(c, sup)
+        if sup >= minsup:
+            freq.append((c,sup))
+    return freq
+
+def candidate_gen(f, index):
+    can = []
+    if index == 0:
+        for i in range(len(frequent[index])):
+            for j in range(i+1, len(frequent[index])):
+                (c1, _) = frequent[index][i]
+                (c2, _) = frequent[index][j]
+                can.append(c1.union(c2))
+    else: 
+        for i in range(len(frequent[index])):
+            for j in range(i+1, len(frequent[index])):
+                (c1, _) = frequent[index][i]
+                (c2, _) = frequent[index][j]
+                #print(c1, c2)
+                if len(c1.difference(c2)) == 1:
+                    if c1.union(c2) not in can:
+                        can.append(c1.union(c2))
+    return can
+
+def list_to_set(lst):
+    my_set = set()
+    for i in lst:
+        my_set.add(i)
+    return my_set
